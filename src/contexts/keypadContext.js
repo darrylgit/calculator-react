2d61e5ed116260f5dccf8543961f540eedebf76d
darrylgit <darrylsmithup@gmail.com>
2020-05-30 12:10:39 -0400

    Add constants for operators

--- src/contexts/keypadContext.js
+++ src/contexts/keypadContext.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
+import { DIVIDE, MULTIPLY, SUBTRACT, ADD } from '../constants';
+
 // Prepare array of input key data for iteration within component
 const negativeLabel = (
   <div>
@@ -25,19 +27,19 @@ const inputKeys = [
     'util'
   ),
   new InputKeyFactory('parentheses', '( )', 'util'),
-  new InputKeyFactory('divide', String.fromCharCode(247), 'operator'),
+  new InputKeyFactory(DIVIDE, DIVIDE, 'operator'),
   new InputKeyFactory(7, '7', 'num'),
   new InputKeyFactory(8, '8', 'num'),
   new InputKeyFactory(9, '9', 'num'),
-  new InputKeyFactory('multiply', String.fromCharCode(215), 'operator'),
+  new InputKeyFactory(MULTIPLY, MULTIPLY, 'operator'),
   new InputKeyFactory(4, '4', 'num'),
   new InputKeyFactory(5, '5', 'num'),
   new InputKeyFactory(6, '6', 'num'),
-  new InputKeyFactory('subtract', String.fromCharCode(8722), 'operator'),
+  new InputKeyFactory(SUBTRACT, SUBTRACT, 'operator'),
   new InputKeyFactory(1, '1', 'num'),
   new InputKeyFactory(2, '2', 'num'),
   new InputKeyFactory(3, '3', 'num'),
-  new InputKeyFactory('add', String.fromCharCode(43), 'operator'),
+  new InputKeyFactory(ADD, ADD, 'operator'),
   new InputKeyFactory('negative', negativeLabel, 'util'),
   new InputKeyFactory(0, '0', 'num'),
   new InputKeyFactory('decimal', '.', 'util'),
--- src/logic/newValueHandler/newValueHandler.js
+++ src/logic/newValueHandler/newValueHandler.js
@@ -1,28 +1,12 @@
-const isNatural = val => {
-  const naturalNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  return naturalNumbers.includes(val);
-};
-
-// export default (currentValues, newValue) => {
-
-//   let values = [...currentValues];
-
-//   if (isNatural(newValue)) {
-//     let lastValue = values.pop();
-//     lastValue *= 10;
-//     lastValue += newValue;
-//     values.push(lastValue);
-
-//     return values;
-//   }
-
-//   return [...values, newValue];
+// const isNatural = val => {
+//   const naturalNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   return naturalNumbers.includes(val);
 // };
 
 export default (currentValues, newValue) => {
   let values = [...currentValues];
 
-  if (isNatural(newValue)) {
+  if (typeof newValue == 'number') {
     let lastValue = values.pop() || 0;
     lastValue *= 10;
     lastValue += newValue;
