1d1e96ae9787fb893a8f0ca51aa5d1877d604e9b
darrylgit <darrylsmithup@gmail.com>
2020-06-03 13:39:40 -0400

    Hide redundant times signs in display

--- src/components/Display/display.component.jsx
+++ src/components/Display/display.component.jsx
@@ -3,13 +3,21 @@ import React, { useContext } from 'react';
 import { DisplayContainer } from './display.styles';
 import CalcContext from '../../contexts/calcContext';
 
+import { OPENPAR, MULTIPLY } from '../../constants';
+
+const displayParser = arr => {
+  return arr
+    .filter((el, i, arr) => !(el === MULTIPLY && arr[i + 1] === OPENPAR))
+    .join('');
+};
+
 const Display = () => {
   const { values } = useContext(CalcContext);
   return (
     <DisplayContainer>
       <div className='outputs'>
         <div className='preview'></div>
-        <div className='main'>{values.join('') || ' '}</div>
+        <div className='main'>{displayParser(values) || ' '}</div>
       </div>
     </DisplayContainer>
   );
