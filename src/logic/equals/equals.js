ad464d4f221a067338f976c679361bffb2f2c2b7
darrylgit <darrylsmithup@gmail.com>
2020-06-04 07:48:28 -0400

    Flesh out tests for equals function

--- src/logic/equals/equals.js
+++ src/logic/equals/equals.js
@@ -1,18 +1,23 @@
 import operations from '../../utils/operations';
 import isOperator from '../../utils/isOperator';
+import { MULTIPLY, DIVIDE, SUBTRACT, ADD } from '../../constants';
 
 const equals = values => {
-  values.forEach((val, i) => {
-    if (isOperator(val)) {
-      operations.multiplyAndDivide(values, i);
-    }
-  });
+  while (values.includes(MULTIPLY) || values.includes(DIVIDE)) {
+    values.forEach((val, i) => {
+      if (isOperator(val)) {
+        operations.multiplyAndDivide(values, i);
+      }
+    });
+  }
 
-  values.forEach((val, i) => {
-    if (isOperator(val)) {
-      operations.addAndSubtract(values, i);
-    }
-  });
+  while (values.includes(ADD) || values.includes(SUBTRACT)) {
+    values.forEach((val, i) => {
+      if (isOperator(val)) {
+        operations.addAndSubtract(values, i);
+      }
+    });
+  }
 
   return values;
 };
--- src/logic/equals/equals.test.js
+++ src/logic/equals/equals.test.js
@@ -14,4 +14,23 @@ it('handles simple calculations', () => {
   expect(equals(['6', SUBTRACT, '4'])).toEqual(['2']);
   expect(equals(['7', MULTIPLY, '8'])).toEqual(['56']);
   expect(equals(['1000', DIVIDE, '2'])).toEqual(['500']);
+  expect(equals(['5', SUBTRACT, '2', ADD, '45'])).toEqual(['48']);
+  expect(equals(['5', MULTIPLY, '10', DIVIDE, '2'])).toEqual(['25']);
+  expect(equals(['546', MULTIPLY, '0'])).toEqual(['0']);
+});
+
+it('observes order of operations', () => {
+  expect(equals(['100', SUBTRACT, '2', MULTIPLY, '20'])).toEqual(['60']);
+  expect(equals(['5', ADD, '1000', DIVIDE, '4'])).toEqual(['255']);
+});
+
+it('handles decimals', () => {
+  expect(equals(['5.2', ADD, '7'])).toEqual(['12.2']);
+  expect(equals(['6.3', MULTIPLY, '2'])).toEqual(['12.6']);
+  expect(equals(['0.2', DIVIDE, '2'])).toEqual(['0.1']);
+});
+
+it('handles negatives', () => {
+  expect(equals(['-3', SUBTRACT, '3'])).toEqual(['-6']);
+  expect(equals(['25', DIVIDE, '-5'])).toEqual(['-5']);
 });
