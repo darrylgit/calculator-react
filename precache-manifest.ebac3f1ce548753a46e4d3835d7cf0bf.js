96f04d6d09463690f4053228b6f8314eb377adf5
darrylgit <darrylsmithup@gmail.com>
2020-06-17 10:17:06 -0400

    Updates

---
+++ precache-manifest.ebac3f1ce548753a46e4d3835d7cf0bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb3aa8754945fda8c2947cfbb9af689",
+    "url": "./index.html"
+  },
+  {
+    "revision": "16b37b5739e7eb6e0ccd",
+    "url": "./static/css/main.64ed331e.chunk.css"
+  },
+  {
+    "revision": "9fc85a30ed5cbbe2b4d2",
+    "url": "./static/js/2.a92a94f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a92a94f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16b37b5739e7eb6e0ccd",
+    "url": "./static/js/main.306b7a13.chunk.js"
+  },
+  {
+    "revision": "cf1024527a882e37d61b",
+    "url": "./static/js/runtime-main.6e57a218.js"
+  }
+]);
